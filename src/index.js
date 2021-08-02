f1b32334facb76601bf65b7239b1442317dd6532
XiaoxuanXue <xx94xuan@gmail.com>
2021-08-02 23:04:06 +0800

    add Router and update Header nav

--- src/index.js
+++ src/index.js
@@ -1,15 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './index.css';
-import App from './App';
+import App from './InitApp/App';
+import LoadMore from './LoadMore/LoadMore';
 import reportWebVitals from './reportWebVitals';
 
+let divBlocksContent = []
+for (let i = 0; i < 100; i++) {
+  divBlocksContent.push(i+1);
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+     <div>
+        <ul className='header-nav'>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/test">Test</Link>
+          </li>
+          <li>
+            <Link to="/load-more">LoadMore</Link>
+          </li>
+        </ul>
+
+        <hr />
+      <Switch>
+        <Route exact path="/">
+          <App />
+        </Route>
+        <Route path="/test">
+          <Test />
+        </Route>
+        <Route path="/load-more">
+          <LoadMore divBlocksContent={divBlocksContent} />
+        </Route>
+      </Switch>
+    </div>
+  </Router>,
   document.getElementById('root')
 );
+
+
+function Test() {
+  return (
+    <div>
+      <h2>Test</h2>
+    </div>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
